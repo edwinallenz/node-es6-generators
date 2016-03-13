717d48d69a581c63813974d3c090b8e26c251d43
Fred Allen <fred@yalochat.com>
2016-03-12 20:30:42 -0600

    Object iterator accepts a number

--- objectIterator.js
+++ objectIterator.js
@@ -1,10 +1,11 @@
-module.exports = (someObj) => {
+module.exports = (someObj, maxNum) => {
     var value = 0;
     var done = false;
 
     someObj.next = _ => {
-        if (++value > 10){
+        if (++value > (maxNum == null ? 10 : maxNum) ){
             done = true
+            value = null
         }
         return {value:value, done:done}
     }
